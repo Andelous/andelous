1a3230b305a2ec17e0634aa5782d2caf12111646
Angel Daniel Alonso Tovar <andelous.aa@gmail.com>
2021-06-05 00:31:02 -0500

    Main application code

---
+++ src/main/java/com/angeld/azurehomepage/AzureHomepageApplication.java
@@ -0,0 +1,13 @@
+package com.angeld.azurehomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureHomepageApplication.class, args);
+	}
+
+}
