64c9c432bc012fe85d1e4247bef8a17500c7f433
Angel Daniel Alonso Tovar <andelous.aa@gmail.com>
2021-08-03 16:51:13 -0500

    Trying to resolve the encoding issue

--- src/main/java/com/angeld/azurehomepage/filters/EncodingFilter.java
+++ src/main/java/com/angeld/azurehomepage/filters/EncodingFilter.java
@@ -1,22 +1,18 @@
 package com.angeld.azurehomepage.filters;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.CharacterEncodingFilter;
-
-@Configuration
+// @Configuration
 public class EncodingFilter {
 
-    @Bean
-    public Object method() {
-        CharacterEncodingFilter filter = new CharacterEncodingFilter();
-        filter.setEncoding("UTF-8");
-        filter.setForceEncoding(true);
+    // @Bean
+    // public Object method() {
+    // CharacterEncodingFilter filter = new CharacterEncodingFilter();
+    // filter.setEncoding("UTF-8");
+    // filter.setForceEncoding(true);
 
-        FilterRegistrationBean<CharacterEncodingFilter> registrationBean = new FilterRegistrationBean<CharacterEncodingFilter>();
-        registrationBean.setFilter(filter);
-        registrationBean.addUrlPatterns("/*");
-        return registrationBean;
-    }
+    // FilterRegistrationBean<CharacterEncodingFilter> registrationBean = new
+    // FilterRegistrationBean<CharacterEncodingFilter>();
+    // registrationBean.setFilter(filter);
+    // registrationBean.addUrlPatterns("/*");
+    // return registrationBean;
+    // }
 }
