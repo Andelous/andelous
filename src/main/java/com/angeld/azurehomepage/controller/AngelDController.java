3d0acc8f61dc1c9ba865a7484d99e60763840af9
Angel Daniel Alonso Tovar <andelous.aa@gmail.com>
2021-08-03 11:39:32 -0500

    First pretty draft

--- src/main/java/com/angeld/azurehomepage/controller/AngelDController.java
+++ src/main/java/com/angeld/azurehomepage/controller/AngelDController.java
@@ -1,12 +1,11 @@
 package com.angeld.azurehomepage.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AngelDController {
-    @GetMapping(path = "/**")
-    public String defaultPage() {
-        return "default";
-    }
+    // @GetMapping(path = "/**")
+    // public String defaultPage() {
+    // return "default";
+    // }
 }
