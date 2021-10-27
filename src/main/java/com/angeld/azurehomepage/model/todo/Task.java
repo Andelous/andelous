5bf71e5cfe6ad4cb29063df167d22b0d13b18e21
Angel Daniel Alonso Tovar <andelous.aa@gmail.com>
2021-10-27 18:59:27 -0500

    More changes

--- src/main/java/com/angeld/azurehomepage/model/todo/Task.java
+++ src/main/java/com/angeld/azurehomepage/model/todo/Task.java
@@ -1,6 +1,10 @@
 package com.angeld.azurehomepage.model.todo;
 
+import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Task {
     private Integer id;
@@ -10,8 +14,14 @@ public class Task {
     private String description;
     private TaskStatus status;
 
+    private Integer parentTaskId;
+
     private Task parentTask;
-    private List<Task> childTasks;
+    private Set<Task> childTasks;
+
+    public Task() {
+        childTasks = new LinkedHashSet<>();
+    }
 
     public Integer getId() {
         return id;
@@ -53,6 +63,14 @@ public class Task {
         this.description = description;
     }
 
+    public Integer getParentTaskId() {
+        return parentTaskId;
+    }
+
+    public void setParentTaskId(Integer parentTaskId) {
+        this.parentTaskId = parentTaskId;
+    }
+
     public Task getParentTask() {
         return parentTask;
     }
@@ -61,12 +79,41 @@ public class Task {
         this.parentTask = parenTask;
     }
 
+    /**
+     * Returns an unmodifiable view of the child tasks belonging to this task.
+     * {@link LinkedHashSet} is the underlying implementation of the list containing
+     * the tasks.
+     * 
+     * @return List of tasks belonging to this task.
+     */
     public List<Task> getChildTasks() {
-        return childTasks;
+        return Collections.unmodifiableList(childTasks.stream().collect(Collectors.toList()));
     }
 
-    public void setChildTasks(List<Task> childTasks) {
-        this.childTasks = childTasks;
+    /**
+     * Tries adding the specified task to the list of child tasks.
+     * 
+     * @param childTask Child task to add. Not <code>null</code>.
+     * @return <code>true</code> if the task wasn't already contained in the child
+     *         task list. <code>false</code> otherwise.
+     */
+    public boolean addChildTask(Task childTask) {
+        if (childTask == null)
+            throw new NullPointerException("New child tasks can't be null");
+
+        return childTasks.add(childTask);
     }
 
+    /**
+     * 
+     * @param childTask Child task to remove. Not <code>null</code>.
+     * @return <code>true</code> if the task was contained in the child task list.
+     *         <code>false</code> otherwise.
+     */
+    public boolean removeChildTask(Task childTask) {
+        if (childTask == null)
+            throw new NullPointerException("Can't remove null tasks from the children list");
+
+        return childTasks.remove(childTask);
+    }
 }
--- src/main/java/com/angeld/azurehomepage/model/todo/TaskDAO.java
+++ src/main/java/com/angeld/azurehomepage/model/todo/TaskDAO.java
@@ -49,6 +49,9 @@ public class TaskDAO {
 
     public Task insertTask(Task task) {
 
+        // TODO: Implement
+
+        return task;
     }
 
     // ------------------------------------
